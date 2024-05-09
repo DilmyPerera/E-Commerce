737deec291abd5b9f21c480135334bb1e64ea3db
DilmyPerera <samadiniperera20@gmail.com>
2024-05-09 21:06:52 +0530

    feat: create components of shop page

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import './Hero.css'
+import hand_icon from '../Componenets/Assests/hand_icon.png'
+import arraow_icon from '../Componenets/Assests/arrow.png'
+import hero_image from '../Componenets/Assests/hero_image.png'
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='hero'>
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+            <div className="hand-hand-icon">
+                <p>new</p>
+                <img src={hand_icon} alt="" />
+            </div>
+            <p>collection</p>
+            <p>for everyone</p>
+        </div>    
+            <div className="hero-latest-btn">
+                <div>
+                    Latest Collection
+                </div>
+                <img src={arraow_icon} alt="" />
+            </div>   
+      </div>
+      <div className="hero-right">
+        <img src={hero_image} alt="" />
+      </div>
     </div>
   )
 }
