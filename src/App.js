0b6f709f184838d0d9f6da2c0d689b77b993c2ec
DilmyPerera <samadiniperera20@gmail.com>
2024-05-09 12:01:13 +0530

    feat: setup complete route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 import Navbar from './Componenets/Navbar/Navbar';
 
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ShopCategory from './pages/ShopCategory';
+import Shop from './pages/Shop';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignup from './pages/LoginSignup';
+
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="mens"/>}/>
+        <Route path='/womens' element={<ShopCategory category="womens"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+        
+        
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
