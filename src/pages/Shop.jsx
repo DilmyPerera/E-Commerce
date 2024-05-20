b104f63c1702a619ca85c49bfa84b8ad97598aea
DilmyPerera <samadiniperera20@gmail.com>
2024-05-20 14:18:49 +0530

    feat:

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../Hero/Hero'
 import Popular from '../Componenets/Popular/Popular'
+import Offers from '../Componenets/Offers/Offers'
+import NewCollections from '../Componenets/NewCollections/NewCollections.jsx'
 
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
+      <Offers/>
+      <NewCollections/>
     </div>
   )
 }
