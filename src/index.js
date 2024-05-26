9398263558e8303eb20e1c40197265c278bafa10
DilmyPerera <samadiniperera20@gmail.com>
2024-05-26 09:56:07 +0530

    feat:

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ShopContextProvider from './Contexts/ShopContext';
+import
+ ShopContextProvider from './Contexts/ShopContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
